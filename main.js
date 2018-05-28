880eea67c21e93c4dad927b4b2d4a2e537a6f1cc
Jonas Holbech <jh@jonasholbech.dk>
2018-05-28 10:25:33 +0200

    car moving

--- main.js
+++ main.js
@@ -1,32 +1,44 @@
 window.addEventListener("load", init);
 
 
-let stage, box, circle;
+let stage, car;
 function init(){
     stage = new createjs.Stage("stage");//the id of the canvas
     createjs.Ticker.framerate = 30;
     createjs.Ticker.addEventListener("tick", tock);
     console.log("load event happened");
-    box = new createjs.Shape();
-    box.graphics.beginFill("#bada55");
-    box.graphics.drawRect(0,0, 60, 60);
-    box.x=600/2;
-    box.y=400/2;
-    stage.addChild(box);
 
-    circle = new createjs.Shape();
+    car = new createjs.Container();
+
+    let box = new createjs.Shape();
+    box.graphics.beginFill("#bada55");
+    box.graphics.drawRect(0,0, 100, 60);
+    car.addChild(box);
+
+    let circle = new createjs.Shape();
     circle.graphics.beginFill("#c0ffee");
-    circle.graphics.drawCircle(0,0, 40);
-    circle.x=600/2;
-    circle.y=400/2;
-    stage.addChild(circle);
+    circle.graphics.drawCircle(0,0, 10);
+    circle.x=20;
+    circle.y=60;
+    car.addChild(circle);
+
+    let circle2 = new createjs.Shape();
+    circle2.graphics.beginFill("#c0ffee");
+    circle2.graphics.drawCircle(0,0, 10);
+    circle2.x=80;
+    circle2.y=60;
+    car.addChild(circle2);
+    stage.addChild(car);
 
 }
+//github.com/jofhatkea/
 
 function tock(e){
     //console.log("redraw")
-    box.rotation++;
-    circle.rotation++;
+    car.x+=10;
+    if(car.x>600){
+        car.x=-100;
+    }
     stage.update(e);
 
 }
